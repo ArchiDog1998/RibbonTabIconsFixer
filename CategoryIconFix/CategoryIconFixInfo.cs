18a2664b8130e68e4f24b988a46727eb1391cc91
秋水 <1123993881@qq.com>
2021-11-26 22:05:00 +0800

    Add project files.

---
+++ CategoryIconFix/CategoryIconFixInfo.cs
@@ -0,0 +1,26 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace CategoryIconFix
+{
+    public class CategoryIconFixInfo : GH_AssemblyInfo
+    {
+        public override string Name => "CategoryIconFix";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("C276B873-B256-4BE9-8B1C-2CA4F7A77BE3");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
